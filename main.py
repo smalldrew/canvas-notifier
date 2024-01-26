850f476a7050abca2b30655db7ce013803ae4405
smalldrew <idk.contact1000@gmail.com>
2024-01-25 17:45:37 -0800

    seperated modules

--- main.py
+++ main.py
@@ -1,81 +1,9 @@
-from dotenv import load_dotenv
-import os
-import requests
-import json
 from datetime import date
+import canvas
+from util.sms import send_text
 
-load_dotenv()
-
-CANVAS_API_TOKEN = os.getenv('CANVAS_API_TOKEN')
-CANVAS_URL = 'https://canvas.instructure.com/api/v1'
-HEADER = {'Authorization': f'Bearer {CANVAS_API_TOKEN}', }
-
-
-def get_user_id() -> int:
-    """Queries Canvas for user id"""
-    user_id_url = f'{CANVAS_URL}/users/self'
-    response = requests.get(url=user_id_url, headers=HEADER) 
-
-    if response.status_code == 200:
-        data = response.json() 
-        return data["id"]
-
-    return -1
-
-
-def get_course_list() -> list[dict]:
-    """Returns a list of courses from the user"""
-    courses_url = f'{CANVAS_URL}/courses'
-    course_params = {
-        'enrollment_type': 'student',
-        'enrollment_state':'active',
-    }
-    course_list = []
-
-    request = requests.get(url=courses_url, params=course_params, headers=HEADER)
-
-    if request.status_code == 200:
-        course_data = request.json()
-
-        for course in course_data:
-            curr_course_data = {
-                'id': course['id'],
-                'name': course['name'],
-                'time_zone': course['time_zone'],
-
-            }
-
-            course_list.append(curr_course_data)
-
-    return course_list
-
-
-def unsubmitted_course_assignments(course: dict):
-    """Gets the assignment due dates of a course"""
-    course_due_date_url = f'{CANVAS_URL}/courses/{course["id"]}/assignments'
-    assignment_params = {
-        'bucket': 'unsubmitted',
-    }
-
-    request = requests.get(url=course_due_date_url, headers=HEADER, params=assignment_params)
-    assignment_list = []
-
-    if request.status_code == 200:
-        data = request.json()
-        for assignment in data:
-            if assignment['due_at'] is None:
-                continue
-
-            curr_assignment_data = {
-                'name' : assignment['name'],
-                'points_possible': assignment['points_possible'],
-                'due_at': assignment['due_at'],
-            }
-            assignment_list.append(curr_assignment_data)
-
-    course['assignments'] = assignment_list
-    return course
-
+def main():
+    pass
 
 if __name__ == '__main__':
-    pass
+    main()
