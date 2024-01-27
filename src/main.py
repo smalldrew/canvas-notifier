3efa993767f18688b325edf0b44ef962d30af0b1
smalldrew <idk.contact1000@gmail.com>
2024-01-27 14:11:18 -0800

    update

--- src/main.py
+++ src/main.py
@@ -3,39 +3,45 @@ from src import canvas
 from util.sms import send_text
 import pytz
 
-BUFFER_MINUTES = 240  # minutes before assignment is due
+# Warning Schedule: [1 day, 6 hours, 3 hours, 1 hour, 30 minutes, 10 minutes)
+WARNING_SCHEDULE = [1440, 360, 180, 60, 30, 10]
 
-def time_remaining(given_date) -> float:
-    """Returns the time remaining for the given assignment"""
+
+def time_remaining(given_date) -> int:
+    """Returns the time remaining for the given assignment in minutes"""
     current_time_utc = datetime.now(pytz.timezone('UTC'))  # Get current time in UTC format
     assignment_deadline = datetime.strptime(given_date, '%Y-%m-%dT%H:%M:%SZ')
     assignment_deadline = pytz.utc.localize(assignment_deadline)
 
     remaining_time = assignment_deadline - current_time_utc
 
-    return remaining_time.total_seconds() / 60
+    return int(remaining_time.total_seconds() / 60)
 
 
-def days_hours_minutes_format(minutes):
+def days_hours_minutes_format(minutes) -> str:
+    """Returns a format of Days, Hours, and Minutes"""
     days = minutes // (24 * 60)
     hours = (minutes % (24 * 60)) // 60
     remaining = minutes % 60
 
     return f'{days} Days, {hours} Hours, {remaining} Minutes'
 
-def course_alert(course):
-    """Sends an alert text to """
-    send_text('hahaha')
-    print(f'Text sent: bruh')
 
-def check_course_due(ba):
-    """Checks if the course"""
+def course_alert(course: dict, assignment: dict) -> None:
+    """Sends an alert text to the user"""
+    assignment_due_in = days_hours_minutes_format(time_remaining(assignment["due_at"]))
+    send_text(f'CANVAS ALERT: Assignment Due In: {assignment_due_in}')
+    send_text(f'Assignment: {assignment["name"]} | Points: {assignment["points_possible"]}')
+    send_text(f'Class: {course["name"]}')
+
+    print(f'Sent Assignment Alert!')
 
 
 def main():
     """Do a full check on Canvas assignments and send a text if necessary."""
     print('Getting a courses from Canvas...')
     course_list = canvas.get_course_list()
+
 
     if course_list:
         print('Courses found! \n')
@@ -50,16 +56,17 @@ def main():
             print('No assignments found.')
 
         for assignment in course["assignments"]:
-            time_left = days_hours_minutes_format(int(time_remaining(assignment["due_at"])) )
-            print(f'  - {assignment["name"]} | Due In: {time_left} | Points: {assignment["points_possible"]}')
+            minutes_remaining = time_remaining(assignment["due_at"])
+            time_left_formatted = days_hours_minutes_format(minutes_remaining)
+
+            print(f'  - {assignment["name"]} | Due In: {time_left_formatted} | Points: {assignment["points_possible"]}')
+
+            if minutes_remaining in WARNING_SCHEDULE:
+                course_alert(course, assignment)
 
         print()
-        # if check_course_due(course):
-        #     course_alert(course)
 
-
-    # print(course_list)
-
+    course_alert(course_list[2], course_list[2]["assignments"][0])
 
 if __name__ == '__main__':
     main()
